79ba3115673339d77241e5213f9a2c6cd4a5a405
AhmedEl-Abbasy <AhmedEl-Abbasy00@github.com>
2021-01-07 06:28:36 +0200

    upload test code to assume H,N,Y

---
+++ test.m
@@ -0,0 +1,22 @@
+%X 
+%No 
+%fs 
+%L=10;
+
+%To assume H 
+h=complex(rand(1,L));
+H=zeros(L);
+for i=1:1:L
+    H(i,i)=h(1,1);
+    if i>1
+        for j=1:1:i-1
+            H(i,i-j)=h(1,j+1);
+        end
+    end
+end
+
+%To assume AWGN Noise 
+N=(No/2)randn(size(X));
+
+%To compute Output Y 
+Y=H*X+N
